93068aa8cde097cb832622241e3e8f2a2ece9965
Leandro <leandro.lisboa.1@outlook.com>
2022-08-17 20:13:20 -0300

    first commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.Write("Informe um número: ");
+    int numero = int.Parse(Console.ReadLine());
+    
+    Console.WriteLine("O número antecessor de {0} é {1} e o sucessor é {2}.", numero, numero - 1, numero + 1);
+  }
+}
